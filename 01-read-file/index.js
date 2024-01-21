f29148939e7a2b1e17bfd0dc673c17028b951af4
Gleb <glebbong12387@gmail.com>
2024-01-21 17:55:02 +0200

    feat: implement writeable functional

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
-// const filePath = path.join(__dirname, '01-read-file', 'text.txt');
+// const filePath = path.join(__dirname, 'text.txt');
 
-fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+// const readStream = fs.createReadStream(filePath, 'utf-8');
+
+// readStream.on('data', (data) => {
+//   console.log(data);
+// });
---
+++ 02-write-file/index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const readline = require('readline');
+const { createWriteStream } = require('fs');
+const { stdin: input, stdout: output } = require('process');
+
+const outputFilePath = path.join(__dirname, 'text.txt');
+const writeStream = createWriteStream(outputFilePath);
+
+const rl = readline.createInterface({
+  input,
+  output,
+});
+
+console.log(
+  'Hi! Enter your card number, date, cvv code, or type "exit" to end the process.',
+);
+
+const userInputHandler = (input) => {
+  if (input.toLowerCase() === 'exit') {
+    console.log('Exiting. Goodbye!');
+    rl.close();
+    writeStream.end();
+  } else {
+    writeStream.write(input + '\n');
+    rl.question('', userInputHandler);
+  }
+};
+
+rl.question('', userInputHandler);
+
+rl.on('close', () => {
+  console.log('Process ended. Goodbye!');
+  writeStream.end();
+});
